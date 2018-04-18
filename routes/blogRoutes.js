99e2cfa81e8a73d47f2058c61a7e78da06efa457
VaRoX301X <elvarox301@gmail.com>
2018-04-18 23:43:17 +0200

    Added Edit Route
    Added edit Route & different links.
    Used MethodOverride

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -19,19 +19,49 @@ router.get("/", function(req, res){
 router.get("/new", function(req,res){
 	res.render("blogs/new");
 })
-//CREATE - ADD A NEW CAMPGROUND
+//CREATE - ADD A NEW blog
 router.post("/", function(req,res){
 	req.body.blog.content = req.sanitize(req.body.blog.content);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if (err) {
 			console.log(err);
-			res.render("new");
+			res.render("blogs/new");
 		} else {
 			res.redirect("/blog");
 		}
 	});
 });
-
+//SHOW BLOG ENTRY
+router.get("/:id", function(req, res){
+    //find the blog with provided ID
+    Blog.findById(req.params.id).populate("comments").exec(function(err, foundblog){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundblog)
+            //render show template with that blog
+            res.render("blogs/show", {blog: foundblog});
+        }
+    });
+});
+//EDIT BLOG ENTRY
+router.get("/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundblog){
+        res.render("blogs/edit", {blog: foundblog});
+    });
+});
+//UPDATE ROUTE
+router.put("/:id", function(req, res){
+    // find and update the correct blog
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedblog){
+       if(err){
+           res.redirect("/blog");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/blog/" + req.params.id);
+       }
+    });
+});
 
 
 
